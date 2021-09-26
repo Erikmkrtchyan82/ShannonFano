4fb9c9ad5af3700210e5fefd726ab62f14ea8edc
Erik <erik.mkrtchyan82@gmail.com>
2021-09-26 12:38:47 +0400

    first commit

---
+++ cmake-build-debug/CMakeFiles/Shannon_Fano.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shannon_Fano.dir/S.cpp.obj"
+  "CMakeFiles/Shannon_Fano.dir/main.cpp.obj"
+  "Shannon_Fano.exe"
+  "Shannon_Fano.exe.manifest"
+  "Shannon_Fano.pdb"
+  "libShannon_Fano.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shannon_Fano.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
